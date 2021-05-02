16e342af722cad2d86356926fd4b8169180cba30
Hamza Hasbi <hamza.hasbi@gmail.com>
2021-05-02 20:54:50 +0000

    init new beginner contest

---
+++ Codeforces/UTPC Contest 04-02-21 Div. 2 (Beginner)/problemA.cpp
@@ -0,0 +1,43 @@
+/*
+ Author: Hamza Hasbi
+ @Copyrights: __Morty
+*/
+#include<bits/stdc++.h>
+#ifdef ONLINE_JUDGE
+ #pragma GCC optimize("O3")
+#endif
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define uld unsigned long double
+#define ud unsigned double
+#define uf unsigned float
+#define pi 2 * acos(0.0)
+#define module cin.ignore()
+//inline long long lcm(ll a,ll b) {return a * b / __gcd(a,b);}
+//inline long long gcd(ll a,ll b) {return 1LL * b == 0 ? a : gcd(1LL*b, a * 1LL % b * 1LL);}
+inline bool IsPowerOfTwo (ull x){ return (x != 0) && ((x & (x - 1)) == 0);}
+using namespace std;
+void READ() {
+ #ifndef ONLINE_JUDGE
+   freopen("io/input.in", "r", stdin);
+   freopen("io/output.out", "w", stdout);
+  #endif
+}
+int main() {
+    auto start = std::chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+//    READ();
+    
+    int n;
+    cin >> n;
+
+    vector<string>s(n);
+    cin.ignore();
+
+    for(int i = 0; i < n; ++i) getline(cin, s[i]);
+
+    for(int i = n - 1; i >= 0; --i) cout << s[i] << endl;
+    return 0;
+}
