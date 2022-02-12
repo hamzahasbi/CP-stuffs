6439147ed656ad467347c0d661df1f2a3ea9b35e
hamzahasbi <hamza.hasbi@gmail.com>
2022-02-12 11:24:36 +0100

    Add: contest problem"

---
+++ Kattis/egypt.cpp
@@ -0,0 +1,47 @@
+/*
+ Author: Hamza Hasbi
+ @Copyrights: __Morty
+*/
+#include<bits/stdc++.h>
+#ifdef ONLINE_JUDGE
+ #pragma GCC optimize("O3")
+#endif
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define uld unsigned long double
+#define ud unsigned double
+#define uf unsigned float
+#define pi 2 * acos(0.0)
+#define module cin.ignore()
+//inline long long lcm(ll a,ll b) {return a * b / __gcd(a,b);}
+//inline long long gcd(ll a,ll b) {return 1LL * b == 0 ? a : gcd(1LL*b, a * 1LL % b * 1LL);}
+inline bool IsPowerOfTwo (ull x){ return (x != 0) && ((x & (x - 1)) == 0);}
+using namespace std;
+void READ() {
+ #ifndef ONLINE_JUDGE
+   freopen("io/input.in", "r", stdin);
+   freopen("io/output.out", "w", stdout);
+  #endif
+}
+int main() {
+    auto start = std::chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+//    READ();
+    vector<int>a(3);
+    while(cin >> a[0] >> a[1] >> a[2]) {
+      long long sum = accumulate(a.begin(), a.end(), 0);
+      if (sum == 0LL) return 0;
+      sort(a.rbegin(), a.rend());
+
+      sum = 1LL * a[1] * a[1] + a[2] * a[2];
+      if (sum == (1LL * a[0] * a[0])) {
+        puts("right");
+      } else puts("wrong");
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>( end - start ).count();
+    // cout << "Finished in " << duration << " ms" << endl;
+    return 0;
+}
