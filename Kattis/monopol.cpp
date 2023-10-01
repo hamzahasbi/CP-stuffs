216139727aee4812d944cf19e31121933c9a00d8
Hamza Hasbi <hamza.hasbi@gmail.com>
2023-10-01 08:36:25 +0100

    Add: kattis

---
+++ Kattis/monopol.cpp
@@ -0,0 +1,49 @@
+/*
+ Author: Hamza Hasbi
+ @Copyrights: __Morty
+*/
+#include<bits/stdc++.h>
+#ifdef ONLINE_JUDGE
+ #pragma GCC optimize("O3")
+#endif
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define uld unsigned long double
+#define ud unsigned double
+#define uf unsigned float
+#define pi 2 * acos(0.0)
+#define module cin.ignore()
+//inline long long lcm(ll a,ll b) {return a * b / __gcd(a,b);}
+//inline long long gcd(ll a,ll b) {return 1LL * b == 0 ? a : gcd(1LL*b, a * 1LL % b * 1LL);}
+inline bool IsPowerOfTwo (ull x){ return (x != 0) && ((x & (x - 1)) == 0);}
+using namespace std;
+void READ() {
+ #ifndef ONLINE_JUDGE
+   freopen("io/input.in", "r", stdin);
+   freopen("io/output.out", "w", stdout);
+  #endif
+}
+int main() {
+    auto start = std::chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+//    READ();
+    int n;
+    cin >> n;
+    int dice;
+    int probs[11] = {1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1};
+    double res = 0.0;
+    for(int i = 0; i < n; ++i) {
+      cin >> dice;
+      // probability of getting a sum x is x - 1
+      // we need to shift the index by 1 since array is 0 based.
+      dice -= 1;
+      res += (probs[dice - 1] * 1.0) / 36;
+    }
+    printf("%.5f\n", res);
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>( end - start ).count();
+    // cout << "Finished in " << duration << " ms" << endl;
+    return 0;
+}
