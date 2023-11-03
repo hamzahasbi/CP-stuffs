91fd74b9620eec30181c29fad9c2eaaeb7b9485b
Hamza Hasbi <hamza.hasbi@gmail.com>
2023-11-03 20:53:16 +0100

    Add: new cses

---
+++ cses/Sorting and Searching/Collecting_Numbers.cpp
@@ -0,0 +1,57 @@
+/*
+ Author: Hamza Hasbi
+ @Copyrights: __Morty
+*/
+#include<bits/stdc++.h>
+#ifdef ONLINE_JUDGE
+ #pragma GCC optimize("O3")
+#endif
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define uld unsigned long double
+#define ud unsigned double
+#define uf unsigned float
+#define pi 2 * acos(0.0)
+#define module cin.ignore()
+//inline long long lcm(ll a,ll b) {return a * b / __gcd(a,b);}
+//inline long long gcd(ll a,ll b) {return 1LL * b == 0 ? a : gcd(1LL*b, a * 1LL % b * 1LL);}
+inline bool IsPowerOfTwo (ull x){ return (x != 0) && ((x & (x - 1)) == 0);}
+using namespace std;
+void READ() {
+ #ifndef ONLINE_JUDGE
+   freopen("io/input.in", "r", stdin);
+   freopen("io/output.out", "w", stdout);
+  #endif
+}
+int main() {
+    auto start = std::chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+//    READ();
+    int n;
+    cin >> n;
+    vector<int>a(n + 1);
+    for(int i = 1; i <= n; ++i) {
+      int _;
+      cin >> _;
+      a[_] = i;
+    }
+    // sort(a.begin() + 1, a.end());
+    int last = 0, step = 0;
+
+    for(int i = 1; i <= n; ++i) {
+      int j = i + 1;
+      while(a[i] < a[j] && j <= n && i <= n) {
+        j++;i++;
+      }
+      // cout << i  << " " << a[i] << " " << j << " " << a[j] << endl;
+      i = j - 1;
+      step += 1;
+    }
+    cout << step << endl;
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>( end - start ).count();
+    // cout << "Finished in " << duration << " ms" << endl;
+    return 0;
+}
