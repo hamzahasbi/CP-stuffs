f4d5624b18891ffd86176a9a791f45c9e8c1d931
Hamza Hasbi <hamza.hasbi@gmail.com>
2023-12-24 20:28:11 +0100

    Add training sheet

---
+++ Codeforces/381A.cpp
@@ -0,0 +1,66 @@
+/*
+ Author: Hamza Hasbi
+ @Copyrights: __Morty
+*/
+#include<bits/stdc++.h>
+#ifdef ONLINE_JUDGE
+ #pragma GCC optimize("O3")
+ #pragma GCC optimize("Ofast")
+ #pragma GCC optimize("unroll-loops")
+ #pragma GCC target("sse,sse2,sse3,ssse3,sse4")
+#endif
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define uld unsigned long double
+#define ud unsigned double
+#define uf unsigned float
+#define pi 2 * acos(0.0)
+#define module cin.ignore()
+inline long long gcd(ll a,ll b) {return 1LL * b == 0 ? a : gcd(1LL*b, a * 1LL % b * 1LL);}
+inline long long lcm(ll a,ll b) {return a * b / gcd(a,b);}
+inline bool IsPowerOfTwo (ull x){ return (x != 0) && ((x & (x - 1)) == 0);}
+using namespace std;
+void READ() {
+ #ifndef ONLINE_JUDGE
+   freopen("io/input.in", "r", stdin);
+   freopen("io/output.out", "w", stdout);
+  #endif
+}
+int main() {
+    auto start = std::chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+//    READ();
+    int n;
+    cin >> n;
+    vector<int>a(n);
+    for(int i = 0; i < n; ++i) {
+      cin >> a[i];
+    }
+    int x = 0, y = 0;
+    bool turn = 0;
+    while(a.size()) {
+      int r = a.back();
+      int l = a.front();
+      int round = 0;
+      if (l > r) {
+          round += l;
+          a.erase(a.begin());
+        } else {
+          round += r;
+          a.pop_back();
+        }
+      if (turn) {
+        y += round;
+      } else {
+        x += round;
+      }
+      turn ^= 1;
+    }
+    cout << x << " " << y << endl;
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>( end - start ).count();
+    //cout << "Finished in " << duration << " ms" << endl;
+    return 0;
+}
---
+++ Codeforces/9A.cpp
@@ -0,0 +1,45 @@
+/*
+ Author: Hamza Hasbi
+ @Copyrights: __Morty
+*/
+#include<bits/stdc++.h>
+#ifdef ONLINE_JUDGE
+ #pragma GCC optimize("O3")
+ #pragma GCC optimize("Ofast")
+ #pragma GCC optimize("unroll-loops")
+ #pragma GCC target("sse,sse2,sse3,ssse3,sse4")
+#endif
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define uld unsigned long double
+#define ud unsigned double
+#define uf unsigned float
+#define pi 2 * acos(0.0)
+#define module cin.ignore()
+inline long long gcd(ll a,ll b) {return 1LL * b == 0 ? a : gcd(1LL*b, a * 1LL % b * 1LL);}
+inline long long lcm(ll a,ll b) {return a * b / gcd(a,b);}
+inline bool IsPowerOfTwo (ull x){ return (x != 0) && ((x & (x - 1)) == 0);}
+using namespace std;
+void READ() {
+ #ifndef ONLINE_JUDGE
+   freopen("io/input.in", "r", stdin);
+   freopen("io/output.out", "w", stdout);
+  #endif
+}
+int main() {
+    auto start = std::chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+//    READ();
+    vector<int>dice = {1, 2, 3, 4, 5, 6};
+    int x,y;
+    cin >> x >> y;
+    int need = lower_bound(dice.begin(), dice.end(), max(x, y)) - dice.begin();
+    int prob = 6 - need;
+    cout << prob / __gcd(6, prob) << "/" << 6 / __gcd(6, prob) << endl;
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>( end - start ).count();
+    //cout << "Finished in " << duration << " ms" << endl;
+    return 0;
+}
