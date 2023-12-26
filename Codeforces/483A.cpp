92d43197bec74704fd90512b35604d7d474f2aef
Hamza Hasbi <hamza.hasbi@gmail.com>
2023-12-26 22:32:38 +0100

    Add: WP on the sheet group A

---
+++ Codeforces/483A.cpp
@@ -0,0 +1,50 @@
+/*
+ Author: Hamza Hasbi
+ @Copyrights: __Morty
+*/
+#include<bits/stdc++.h>
+#ifdef ONLINE_JUDGE
+ #pragma GCC optimize("O3")
+ #pragma GCC optimize("Ofast")
+ #pragma GCC optimize("unroll-loops")
+ #pragma GCC target("sse,sse2,sse3,ssse3,sse4")
+#endif
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define uld unsigned long double
+#define ud unsigned double
+#define uf unsigned float
+#define pi 2 * acos(0.0)
+#define module cin.ignore()
+inline long long gcd(ll a,ll b) {return 1LL * b == 0 ? a : gcd(1LL*b, a * 1LL % b * 1LL);}
+inline long long lcm(ll a,ll b) {return a * b / gcd(a,b);}
+inline bool IsPowerOfTwo (ull x){ return (x != 0) && ((x & (x - 1)) == 0);}
+using namespace std;
+void READ() {
+ #ifndef ONLINE_JUDGE
+   freopen("io/input.in", "r", stdin);
+   freopen("io/output.out", "w", stdout);
+  #endif
+}
+int main() {
+    auto start = std::chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+//    READ();
+    ll l,r;
+    cin >> l >> r;
+    if (r - l + 1 < 3) cout << -1 << endl;
+    else {
+      if (l & 1) {
+        if (l + 3 <= r) cout << l + 1 << " "<< l + 2 << " " << l + 3 << endl;
+        else cout << -1 << endl;
+      } else {
+        cout << l << " "<< l + 1 << " " << l + 2 << endl;
+      }
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>( end - start ).count();
+    //cout << "Finished in " << duration << " ms" << endl;
+    return 0;
+}
