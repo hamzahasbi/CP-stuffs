3b641bc4489b94cf1b3755c8728bdf2490202683
Hamza Hasbi <hamza.hasbi@gmail.com>
2023-12-27 22:19:48 +0100

    Add: WP on the sheet group A

---
+++ Codeforces/365A.cpp
@@ -0,0 +1,57 @@
+/*
+ Author: Hamza Hasbi
+ @Copyrights: __Morty
+*/
+#include<bits/stdc++.h>
+#ifdef ONLINE_JUDGE
+ #pragma GCC optimize("O3")
+ #pragma GCC optimize("Ofast")
+ #pragma GCC optimize("unroll-loops")
+ #pragma GCC target("sse,sse2,sse3,ssse3,sse4")
+#endif
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define uld unsigned long double
+#define ud unsigned double
+#define uf unsigned float
+#define pi 2 * acos(0.0)
+#define module cin.ignore()
+inline long long gcd(ll a,ll b) {return 1LL * b == 0 ? a : gcd(1LL*b, a * 1LL % b * 1LL);}
+inline long long lcm(ll a,ll b) {return a * b / gcd(a,b);}
+inline bool IsPowerOfTwo (ull x){ return (x != 0) && ((x & (x - 1)) == 0);}
+using namespace std;
+void READ() {
+ #ifndef ONLINE_JUDGE
+   freopen("io/input.in", "r", stdin);
+   freopen("io/output.out", "w", stdout);
+  #endif
+}
+int main() {
+    auto start = std::chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+//    READ();
+    int n,k;
+    cin >> n >> k;
+    int ans = 0;
+    while(n--) {
+      string s;
+      cin >> s;
+      map<char, int>mp;
+      bool can = true;
+      for(auto el : s) {
+        mp[el] = true;
+      }
+      for(int i = 0; i <= k; ++i) {
+        can &= mp.count(i + '0');
+      }
+      // cout << can << endl;
+      ans += can;
+    }
+    cout << ans << endl;
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>( end - start ).count();
+    //cout << "Finished in " << duration << " ms" << endl;
+    return 0;
+}
