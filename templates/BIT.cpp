5c95f3a71287f38f9242f3177fbd0b8489827858
Hamza Hasbi <hamza.hasbi@gmail.com>
2024-01-01 20:12:24 +0100

    Add: working BIT

--- templates/BIT.cpp
+++ templates/BIT.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-struct FenwickTree {
-    vector<int> bit;  // binary indexed tree
+template <class T> struct FenwickTree {
+    vector<T> bit;  // binary indexed tree
     int n;
 
     FenwickTree(int n) {
@@ -10,19 +10,19 @@ struct FenwickTree {
     }
 
     // O(n * log(n)) Linear construction
-    FenwickTree(vector<int> const &a) : FenwickTree(a.size()) {
+    FenwickTree(vector<T> const &a) : FenwickTree(a.size()) {
         for (size_t i = 0; i < a.size(); i++)
             add(i, a[i]);
     }
 
-    int sum(int r) {
+    T sum(int r) {
         int ret = 0;
         for (; r >= 0; r = (r & (r + 1)) - 1)
             ret += bit[r];
         return ret;
     }
 
-    int sum(int l, int r) {
+    T sum(int l, int r) {
         return sum(r) - sum(l - 1);
     }
 
@@ -31,7 +31,7 @@ struct FenwickTree {
             bit[idx] += delta;
     }
     // O(n) Linear construction
-  //   build(vector<int> const &a) : FenwickTree(a.size()){
+  //   build(vector<T> const &a) : FenwickTree(a.size()){
   //   for (int i = 0; i < n; i++) {
   //       bit[i] += a[i];
   //       int r = i | (i + 1);
@@ -40,9 +40,8 @@ struct FenwickTree {
   // }
 };
 
-
-struct FenwickTreeMin {
-    vector<int> bit;
+template <class T> struct FenwickTreeMin {
+    vector<T> bit;
     int n;
     const int INF = (int)1e9;
 
@@ -51,12 +50,12 @@ struct FenwickTreeMin {
         bit.assign(n, INF);
     }
 
-    FenwickTreeMin(vector<int> a) : FenwickTreeMin(a.size()) {
+    FenwickTreeMin(vector<T> a) : FenwickTreeMin(a.size()) {
         for (size_t i = 0; i < a.size(); i++)
             update(i, a[i]);
     }
 
-    int getmin(int r) {
+    T getmin(int r) {
         int ret = INF;
         for (; r >= 0; r = (r & (r + 1)) - 1)
             ret = min(ret, bit[r]);
@@ -74,7 +73,9 @@ int main() {
     auto start = std::chrono::high_resolution_clock::now();
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
+    int n = 100;
+    FenwickTree<int> BIT (n << 1);
+    FenwickTreeMin<int> BIT (n << 1);
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>( end - start ).count();
     return 0;
