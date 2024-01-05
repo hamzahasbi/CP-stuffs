81df1d3f201a81f6d7106fe0129938686badcab7
Hamza Hasbi <hamza.hasbi@gmail.com>
2024-01-05 19:58:41 +0100

    Add: LIS problem

---
+++ templates/algos.cc
@@ -0,0 +1,100 @@
+/*
+ Author: Hamza Hasbi
+ @Copyrights: __Morty
+*/
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#ifdef ONLINE_JUDGE
+ #pragma GCC optimize("O3")
+ #pragma GCC optimize("Ofast")
+ #pragma GCC optimize("unroll-loops")
+ #pragma GCC target("sse,sse2,sse3,ssse3,sse4")
+ #pragma GCC optimize("Ofast,no-stack-protector,fast-math",3)
+#endif
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define uld unsigned long double
+#define ud unsigned double
+#define uf unsigned float
+#define pi 2 * acos(0.0)
+#define module cin.ignore()
+template <class T> void print_v(vector<T> &v) { cout << '{'; for (auto x : v) cout << x << ','; cout << '}'; cout << endl; }
+inline long long gcd(ll a,ll b) {return 1LL * b == 0 ? a : gcd(1LL*b, a * 1LL % b * 1LL);}
+inline long long lcm(ll a,ll b) {return a * b / gcd(a,b);}
+inline bool IsPowerOfTwo (ull x){ return (x != 0) && ((x & (x - 1)) == 0);}
+void READ() {
+ #ifndef ONLINE_JUDGE
+   freopen("io/input.in", "r", stdin);
+   freopen("io/output.out", "w", stdout);
+  #endif
+}
+template<typename T> void printVector(const T& t) {
+copy(t.cbegin(), t.cend(), ostream_iterator<typename T::value_type>(cout, " "));
+cout << "\n";
+}
+template<typename T> void printVectorInVector(const T& t) {
+for_each(t.cbegin(), t.cend(), printVector<typename T::value_type>);
+}
+void _print() {cout << "]\n";}
+template<typename T> void _print(const T &x) {cout << x;}
+template<typename T, typename... Args>
+void _print(const T &x, const Args &... args) {cout << x << ", "; _print(args...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) do { cout << "[" << #x << "] = ["; _print(x); _print();} while(0)
+#else
+#define debug(x...)
+#endif
+
+template<typename T> class LIS {
+public:
+    int binarySearch(vector<T>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+        ans.push_back(nums[0]);
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > ans.back()) {
+                ans.push_back(nums[i]);
+            }
+            else {
+                int lo = lower_bound(ans.begin(), ans.end(), nums[i]) - ans.begin();
+                ans[lo] = nums[i];
+            }
+
+        }
+        return (int) ans.size();
+    }
+public:
+    int dp(vector<T>& nums) {
+        int n = nums.size();
+        vector<int> a(n, 1);
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int c = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    c = max(c, a[j] + 1);
+                }
+            }
+            a[i] = c;
+
+        }
+        return *max_element(a.begin(), a.end());
+    }
+
+};
+int main() {
+    auto start = std::chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+//    READ();
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>( end - start ).count();
+    //cout << "Finished in " << duration << " ms" << endl;
+    return 0;
+}
