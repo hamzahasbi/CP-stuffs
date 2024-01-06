7162117541cf3a2afcfdc4a098b77d8b3d370d13
Hamza Hasbi <hamza.hasbi@gmail.com>
2024-01-06 21:48:18 +0100

    Add: contest

---
+++ Atcoder/E_Non_Decreasing_Colorful_Path.cpp
@@ -0,0 +1,104 @@
+/*
+ Author: Hamza Hasbi
+ @Copyrights: __Morty
+*/
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#ifdef ONLINE_JUDGE
+ #pragma GCC optimize("O3")
+ #pragma GCC optimize("Ofast")
+ #pragma GCC optimize("unroll-loops")
+ #pragma GCC target("sse,sse2,sse3,ssse3,sse4")
+ #pragma GCC optimize("Ofast,no-stack-protector,fast-math",3)
+#endif
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define uld unsigned long double
+#define ud unsigned double
+#define uf unsigned float
+#define pi 2 * acos(0.0)
+#define module cin.ignore()
+inline long long gcd(ll a,ll b) {return 1LL * b == 0 ? a : gcd(1LL*b, a * 1LL % b * 1LL);}
+inline long long lcm(ll a,ll b) {return a * b / gcd(a,b);}
+inline bool IsPowerOfTwo (ull x){ return (x != 0) && ((x & (x - 1)) == 0);}
+void READ() {
+ #ifndef ONLINE_JUDGE
+   freopen("io/input.in", "r", stdin);
+   freopen("io/output.out", "w", stdout);
+  #endif
+}
+template<typename T> void printVector(const T& t) {
+copy(t.cbegin(), t.cend(), ostream_iterator<typename T::value_type>(cout, " "));
+cout << "\n";
+}
+template<typename T> void printVectorInVector(const T& t) {
+for_each(t.cbegin(), t.cend(), printVector<typename T::value_type>);
+}
+void _print() {cout << "]\n";}
+template<typename T> void _print(const T &x) {cout << x;}
+template<typename T, typename... Args>
+void _print(const T &x, const Args &... args) {cout << x << ", "; _print(args...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) do { cout << "[" << #x << "] = ["; _print(x); _print();} while(0)
+#else
+#define debug(x...)
+#endif
+
+const int MAX_N = 200001;
+unordered_map<int, vector<int>> adj;
+vector<int> vertexValues(MAX_N);
+int maxScore = 0;
+map<int,unordered_set<int>>vis;
+// TLE on some tests
+void dfs(int u, unordered_set<int>& path, int score, int target) {
+    if (u == target) {
+      maxScore = max(maxScore, score);
+      return;
+    }
+    path.insert(vertexValues[u]);
+    // cout << u << " ";
+
+
+    for (int v : adj[u]) {
+        if (vertexValues[v] >= vertexValues[u] && !vis[u].count(v)) {
+            vis[u].insert(v);
+            dfs(v, path, score + (vertexValues[v] != vertexValues[u]), target);
+        }
+    }
+    path.erase(vertexValues[u]);
+}
+
+int main() {
+    auto start = std::chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+//    READ();
+    int N, M;
+    cin >> N >> M;
+
+    for (int i = 1; i <= N; ++i) {
+        cin >> vertexValues[i];
+    }
+
+    for (int i = 0; i < M; ++i) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    unordered_set<int> path;
+    dfs(1, path, 1, N);
+    //cout<< "\n";
+    cout << maxScore << endl;
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>( end - start ).count();
+    //cout << "Finished in " << duration << " ms" << endl;
+    return 0;
+}
