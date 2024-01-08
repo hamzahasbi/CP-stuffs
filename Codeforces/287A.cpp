4f5e229fd32c2780b914ec1e1aff6195e055d20d
Hamza Hasbi <hamza.hasbi@gmail.com>
2024-01-08 21:19:35 +0100

    Add: sheet problems

---
+++ Codeforces/287A.cpp
@@ -0,0 +1,88 @@
+/*
+ Author: Hamza Hasbi
+ @Copyrights: __Morty
+*/
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#ifdef ONLINE_JUDGE
+ #pragma GCC optimize("O3")
+ #pragma GCC optimize("Ofast")
+ #pragma GCC optimize("unroll-loops")
+ #pragma GCC target("sse,sse2,sse3,ssse3,sse4")
+ #pragma GCC optimize("Ofast,no-stack-protector,fast-math",3)
+#endif
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define uld unsigned long double
+#define ud unsigned double
+#define uf unsigned float
+#define pi 2 * acos(0.0)
+#define module cin.ignore()
+inline long long gcd(ll a,ll b) {return 1LL * b == 0 ? a : gcd(1LL*b, a * 1LL % b * 1LL);}
+inline long long lcm(ll a,ll b) {return a * b / gcd(a,b);}
+inline bool IsPowerOfTwo (ull x){ return (x != 0) && ((x & (x - 1)) == 0);}
+void READ() {
+ #ifndef ONLINE_JUDGE
+   freopen("io/input.in", "r", stdin);
+   freopen("io/output.out", "w", stdout);
+  #endif
+}
+template<typename T> void printVector(const T& t) {
+copy(t.cbegin(), t.cend(), ostream_iterator<typename T::value_type>(cout, " "));
+cout << "\n";
+}
+template<typename T> void printVectorInVector(const T& t) {
+for_each(t.cbegin(), t.cend(), printVector<typename T::value_type>);
+}
+void _print() {cout << "]\n";}
+template<typename T> void _print(const T &x) {cout << x;}
+template<typename T, typename... Args>
+void _print(const T &x, const Args &... args) {cout << x << ", "; _print(args...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) do { cout << "[" << #x << "] = ["; _print(x); _print();} while(0)
+#else
+#define debug(x...)
+#endif
+
+bool isValid(int i, int j, int n, int m) {
+  return i >= 0 && j >= 0 && i < n && j < m;
+}
+int main() {
+    auto start = std::chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+//    READ();
+    vector<string>grid;
+    for(int i = 0; i < 4; ++i) {
+      string s;
+      cin >> s;
+      for(auto &cc : s) cc = cc == '#' ? '0' : '1';
+      grid.push_back(s);
+    }
+    const int n = 4;
+    const int m = grid[0].size();
+    bool can = false;
+    int inplace = 0;
+    for(int i = 0; i < n - 1; ++i) {
+      for(int j = 0; j < m - 1; ++j) {
+        int curr = grid[i][j] - '0';
+        int r = j + 1;
+        int d = i + 1;
+        curr += (grid[i][r] - '0') + (grid[d][j] - '0') + (grid[d][r] - '0');
+        inplace += (curr == 4 || curr == 0);
+        can |= (curr == 3 || curr == 1);
+        // cout << "Item => " << curr << endl;
+      }
+    }
+    // cout << inplace << endl;
+    puts(inplace || can ? "YES" : "NO");
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>( end - start ).count();
+    //cout << "Finished in " << duration << " ms" << endl;
+    return 0;
+}
