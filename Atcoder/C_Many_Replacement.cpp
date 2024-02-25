fdf210bf4648217b727773ca44342164d1dd9054
Hamza Hasbi <hamza.hasbi@gmail.com>
2024-02-25 21:15:54 +0100

    Add: problem solving C while testing terminal config

---
+++ Atcoder/C_Many_Replacement.cpp
@@ -0,0 +1,114 @@
+/*
+ Author: Hamza Hasbi
+ @Copyrights: __Morty
+*/
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#ifdef ONLINE_JUDGE
+ #pragma GCC optimize("O3")
+ #pragma GCC optimize("Ofast")
+ #pragma GCC optimize("unroll-loops")
+ #pragma GCC target("sse,sse2,sse3,ssse3,sse4")
+ #pragma GCC optimize("Ofast,no-stack-protector,fast-math",3)
+#endif
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define uld unsigned long double
+#define ud unsigned double
+#define uf unsigned float
+#define pi 2 * acos(0.0)
+#define module cin.ignore()
+inline long long gcd(ll a,ll b) {return 1LL * b == 0 ? a : gcd(1LL*b, a * 1LL % b * 1LL);}
+inline long long lcm(ll a,ll b) {return a * b / gcd(a,b);}
+inline bool IsPowerOfTwo (ull x){ return (x != 0) && ((x & (x - 1)) == 0);}
+void READ() {
+ #ifndef ONLINE_JUDGE
+   freopen("io/input.in", "r", stdin);
+   freopen("io/output.out", "w", stdout);
+  #endif
+}
+template<typename T> void printVector(const T& t) {
+copy(t.cbegin(), t.cend(), ostream_iterator<typename T::value_type>(cout, " "));
+cout << "\n";
+}
+template<typename T> void printVectorInVector(const T& t) {
+for_each(t.cbegin(), t.cend(), printVector<typename T::value_type>);
+}
+void _print() {cout << "]\n";}
+template<typename T> void _print(const T &x) {cout << x;}
+template<typename T, typename... Args>
+void _print(const T &x, const Args &... args) {cout << x << ", "; _print(args...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) do { cout << "[" << #x << "] = ["; _print(x); _print();} while(0)
+#else
+#define debug(x...)
+#endif
+
+template <class F>
+struct y_combinator {
+    F f; // the lambda will be stored here
+
+    // a forwarding operator():
+    template <class... Args>
+    decltype(auto) operator()(Args&&... args) const {
+        // we pass ourselves to f, then the arguments.
+        return f(*this, std::forward<Args>(args)...);
+    }
+};
+
+// helper function that deduces the type of the lambda:
+template <class F>
+y_combinator<std::decay_t<F>> make_y_combinator(F&& f) {
+    return {std::forward<F>(f)};
+}
+
+int main() {
+    auto start = std::chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+//    READ();
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int q;
+    cin >> q;
+    map<char, vector<char>>g;
+    vector<char>x(26);
+    for (int i = 0; i < 26; i++){
+      x[i] = (char) ('a' + i);
+    }
+    while(q--) {
+      char from, to;
+      cin >> from >> to;
+      for(char c = 'a'; c <= 'z'; ++c) {
+        if (x[c - 'a'] == from) {
+          x[c - 'a'] = to;
+        }
+      }
+    }
+    // auto dfs = make_y_combinator([&] (auto self, char node, vector<int> vis) -> char {
+    //   if (g[node].empty()) return node;
+    //   vis[node] = 1;
+    //   for(auto v : g[node]) {
+    //     if (!vis[v - 'a']) self(v, vis);
+    //   }
+    //   return node;
+    // });
+
+
+    for(auto el : s) {
+      cout << x[el - 'a'];
+    }
+    cout << endl;
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>( end - start ).count();
+    //cout << "Finished in " << duration << " ms" << endl;
+    return 0;
+}
